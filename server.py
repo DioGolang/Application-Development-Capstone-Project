9d919306b03722638aa52e83b99cd7648e1ae629
dvasconcelos <di0g0vasconcelos@hotmail.com>
2025-01-24 22:23:05 -0300

    feat: author

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask
+from flask import Flask, escape
 
 app = Flask(__name__)
 
@@ -9,12 +9,23 @@ app = Flask(__name__)
 # 	rating = request.args.get('rating')
 # 	return {"message": f"{course} with rating {rating} has been added"}
 
+# @app.route('/')
+# def get_author():
+# 	res = requests.get('https://openlibrary.org/search/authors.json?q=Michael Crichton')
+# 	if res.status_code == 200:
+# 		return { "message": res.json()}
+# 	elif res.status_code == 404:
+# 		return {"message": "Author not found"}, 404
+# 	else:
+# 		return {"message": "Something went wrong"}, 500
+
 @app.route('/')
-def get_author():
-	res = requests.get('https://openlibrary.org/search/authors.json?q=Michael Crichton')
+def get_author(isbn):
+	res = requests.get(f'https://openlibrary.org/isbn/{escape(isbn)}.json')
 	if res.status_code == 200:
 		return { "message": res.json()}
 	elif res.status_code == 404:
 		return {"message": "Author not found"}, 404
 	else:
 		return {"message": "Something went wrong"}, 500
+
